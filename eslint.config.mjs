6d4e0e635c71243e1f0d0b240476c5d19806e6bb
uvarovag <aleanches@gmail.com>
2025-01-18 12:31:53 +0300

    feat: lint config from package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,3 @@
-import tsParser from '@typescript-eslint/parser'
-import tsPlugin from '@typescript-eslint/eslint-plugin'
-import prettierPlugin from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
+import tsConfig from '@uvarovag/eslint-config-ts'
 
-export default [
-    {
-        ignores: ['node_modules/**', 'dist/**'], // Игнорируем стандартные директории
-    },
-    {
-        files: ['**/*.ts', '**/*.tsx'], // Применяем конфиг к TypeScript-файлам
-        languageOptions: {
-            parser: tsParser, // Указываем TypeScript-парсер
-            ecmaVersion: 'latest', // Последняя версия ECMAScript
-            sourceType: 'module', // Модули ECMAScript
-        },
-        plugins: {
-            '@typescript-eslint': tsPlugin, // Подключаем плагин TypeScript
-            prettier: prettierPlugin, // Подключаем плагин Prettier
-        },
-        rules: {
-            ...tsPlugin.configs.recommended.rules, // Используем рекомендованные правила TypeScript
-            ...prettierConfig.rules,
-        },
-    },
-]
+export default [...tsConfig]
